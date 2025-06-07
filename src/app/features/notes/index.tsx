c9949db6fd53a9df01630f7f6de553c9003f9d2c
hlainghpone1 <hlainghpone1@gmail.com>
2025-06-07 08:10:39 -0700

    add

--- src/app/features/notes/index.tsx
+++ src/app/features/notes/index.tsx
@@ -1,50 +1,65 @@
-import { getAllCategoryNotes } from '@/api/notes';
+import {getAllCategoryNotes} from '@/api/notes';
 import CategoryFolderCard from '@/app/features/notes/components/CategoryFolderCard';
-import { Button } from '@/components/ui/button';
-import { useQuery } from '@tanstack/react-query';
-import { Folder, Plus } from 'lucide-react';
+import {Button} from '@/components/ui/button';
+import {useQuery} from '@tanstack/react-query';
+import {Folder, Plus} from 'lucide-react';
+import {useLocation, useNavigate} from "react-router";
 
 export default function MarkdownEditor() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['note-categories'],
-    queryFn: async (): Promise<NoteCategory[]> =>
-      await getAllCategoryNotes().then((res) => {
-        if (res.data.code === 200) {
-          return res.data.data;
-        }
-        throw new Error('Failed to fetch note categories');
-      }),
-  });
+    const location = useLocation();
 
-  const skeletons = new Array(6).fill(null);
+    const navigate = useNavigate();
 
-  return (
-    <div className="bg-white p-6 rounded-lg shadow-md min-h-[calc(100dvh-220px)]">
-      {isLoading ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
-          {skeletons.map((_, index) => (
-            <div
-              key={index}
-              className="animate-pulse bg-gray-100 rounded-lg h-32 w-full"
-            />
-          ))}
+    const {data, isLoading} = useQuery({
+        queryKey: ['note-categories'],
+        queryFn: async (): Promise<NoteCategory[]> =>
+            await getAllCategoryNotes().then((res) => {
+                if (res.data.code === 200) {
+                    return res.data.data;
+                }
+                throw new Error('Failed to fetch note categories');
+            }),
+    });
+
+    const skeletons = new Array(6).fill(null);
+
+    return (
+        <div className="bg-white p-6 rounded-lg shadow-md min-h-[calc(100dvh-220px)]">
+            {isLoading ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+                    {skeletons.map((_, index) => (
+                        <div
+                            key={index}
+                            className="animate-pulse bg-gray-100 rounded-lg h-32 w-full"
+                        />
+                    ))}
+                </div>
+            ) : (data ?? []).length > 0 ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+                    {data?.map((category) => (
+                        <CategoryFolderCard key={category.categoryId} category={category}/>
+                    ))}
+                </div>
+            ) : (
+                <div className="text-center py-12">
+                    <Folder className="w-16 h-16 text-gray-300 mx-auto mb-4"/>
+                    <p className="text-gray-500 text-lg mb-4">No categories found</p>
+                    <Button className="bg-purple-600 hover:bg-purple-700"
+                            onClick={() =>
+                                navigate('/app/category', {
+                                    state: {
+                                        from: location.pathname,
+                                        categoryTypeId: 4,
+                                        name: "note",
+                                    },
+                                })
+                            }
+                    >
+                        <Plus className="w-4 h-4 mr-2"/>
+                        Create your first category
+                    </Button>
+                </div>
+            )}
         </div>
-      ) : (data ?? []).length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
-          {data?.map((category) => (
-            <CategoryFolderCard key={category.categoryId} category={category} />
-          ))}
-        </div>
-      ) : (
-        <div className="text-center py-12">
-          <Folder className="w-16 h-16 text-gray-300 mx-auto mb-4" />
-          <p className="text-gray-500 text-lg mb-4">No categories found</p>
-          <Button className="bg-purple-600 hover:bg-purple-700">
-            <Plus className="w-4 h-4 mr-2" />
-            Create your first category
-          </Button>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
